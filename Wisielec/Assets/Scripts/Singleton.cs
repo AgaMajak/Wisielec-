69167061491269385d907c068d58000bfd61fed2
AgnieszkaMajak <agamajak@wp.pl>
2022-03-01 22:22:11 +0100

    Added project

---
+++ Wisielec/Assets/Scripts/Singleton.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+
+	private static T _instance;
+
+	public static T instance {
+		get {
+	
+			if (_instance == null)
+			{
+		
+				_instance = FindObjectOfType<T>();    
+
+				if (_instance == null) {
+					Debug.LogError("Nie można znaleźć obiektu tyou " + typeof(T) + "!");
+				}
+			}
+			return _instance;
+		}
+	}
+}
